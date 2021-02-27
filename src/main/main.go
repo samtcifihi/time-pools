e4e778a75f9fc4294e334cedc8a833e211f6c415
samtcifihi <la.lojban@gmail.com>
2021-02-27 09:45:23 -0500

    Implemetned ztime.dec and ztime.inc

--- src/main/main.go
+++ src/main/main.go
@@ -4,10 +4,41 @@ import (
 	"fmt"
 )
 
-var znum = [...]rune{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '\u218A', '\u218B'}
+var znum = [...]string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "\u218A", "\u218B"}
 
 func main() {
-	testTime := ztime{0, 1, 2, 3}
+	fmt.Println("znum test:")
 
-	fmt.Println("Remaining Time:", testTime)
+	for x := 0; x <= 11; x++ {
+		fmt.Println(znum[x])
+	}
+
+	testTime := ztime{0, 0, 1, 3}
+
+	fmt.Println("Initial Time:", testTime)
+
+	fmt.Println("Time left?", testTime.dec(),
+		"\nCurrent Time Remaining", testTime)
+	fmt.Println("Time left?", testTime.dec(),
+		"\nCurrent Time Remaining", testTime)
+	fmt.Println("Time left?", testTime.dec(),
+		"\nCurrent Time Remaining", testTime)
+	fmt.Println("Time left?", testTime.dec(),
+		"\nCurrent Time Remaining", testTime)
+	fmt.Println("Time left?", testTime.dec(),
+		"\nCurrent Time Remaining", testTime)
+	fmt.Println("Time left?", testTime.dec(),
+		"\nCurrent Time Remaining", testTime)
+
+	fmt.Println("Increment Test")
+	testTime.inc()
+	fmt.Println(testTime)
+	testTime.inc()
+	fmt.Println(testTime)
+	testTime.inc()
+	fmt.Println(testTime)
+	testTime.inc()
+	fmt.Println(testTime)
+	testTime.inc()
+	fmt.Println(testTime)
 }
--- src/main/ztime.go
+++ src/main/ztime.go
@@ -2,3 +2,40 @@ package main
 
 // dwells, breathers, trices, lulls
 type ztime [4]byte
+
+func (z *ztime) dec() bool {
+	timeRemaining := true
+
+	if z[3] != 0 {
+		z[3]--
+	} else if z[2] != 0 {
+		z[2]--
+		z[3] = 11
+	} else if z[1] != 0 {
+		z[1]--
+		z[2], z[3] = 11, 11
+	} else if z[0] != 0 {
+		z[0]--
+		z[1], z[2], z[3] = 11, 11, 11
+	} else {
+		timeRemaining = false
+	}
+
+	return timeRemaining
+}
+
+// func inc
+func (z *ztime) inc() {
+	if z[3] != 11 {
+		z[3]++
+	} else if z[2] != 11 {
+		z[2]++
+		z[3] = 0
+	} else if z[1] != 11 {
+		z[1]++
+		z[2], z[3] = 0, 0
+	} else {
+		z[0]++
+		z[1], z[2], z[3] = 0, 0, 0
+	}
+}
